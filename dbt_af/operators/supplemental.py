cd9016ad0ef08412823971968706e9ddc94eb260
Nikita Yurasov <49479190+NikitaYurasov@users.noreply.github.com>
2024-05-31 10:49:53 +0200

    fix tableau operator rendering (#15)
    - fix bug in rendering TableauExtractsRefreshOperator
    - integration tests are run with checking that airflow tasks finished
    successfully

--- dbt_af/operators/supplemental.py
+++ dbt_af/operators/supplemental.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING, Sequence
+from typing import TYPE_CHECKING
 
 from airflow.operators.python import PythonOperator
 
@@ -9,16 +9,22 @@ if TYPE_CHECKING:
     from dbt_af.parser.dbt_node_model import TableauRefreshTaskConfig
 
 
+def _tableau_extracts_refresh_dev(*args, **kwargs) -> None:
+    import logging
+
+    logging.info('tableau_extracts_refresh is disabled in dev mode.')
+    return None
+
+
 class TableauExtractsRefreshOperator(PythonOperator):
-    template_fields: Sequence[str] = ('tableau_refresh_tasks',)
-    template_fields_renderers = {'template_fields_renderers': 'py'}
+    template_fields = tuple()
 
     def __init__(self, tableau_refresh_tasks: 'list[TableauRefreshTaskConfig]', dbt_af_config: Config, **kwargs):
         if not is_tableau_installed():
             raise ImportError('tableauserverclient is not installed. Please install it to use this operator.')
 
         super().__init__(
-            python_callable=tableau_extracts_refresh,
+            python_callable=tableau_extracts_refresh if not dbt_af_config.is_dev else _tableau_extracts_refresh_dev,
             op_kwargs={
                 'tableau_refresh_tasks': tableau_refresh_tasks,
                 'dbt_af_config': dbt_af_config,
