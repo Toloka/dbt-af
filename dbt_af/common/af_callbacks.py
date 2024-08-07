141b4a225c4d424d0a5ab6d7685f819b9136b3af
Nikita Yurasov <n.yurasov@yahoo.com>
2024-08-07 16:36:53 +0200

    feat: small refactoring

--- dbt_af/common/af_callbacks.py
+++ dbt_af/common/af_callbacks.py
@@ -1,9 +1,9 @@
-from typing import Optional
 from collections.abc import Iterable
+from typing import Optional
 
 from dbt_af.conf import Config
-from dbt_af.integrations.mcd import prepare_mcd_callbacks
 from dbt_af.integrations.af_callbacks import prepare_custom_af_callbacks
+from dbt_af.integrations.mcd import prepare_mcd_callbacks
 
 
 def merge_dicts_by_key(*dicts: dict) -> dict:
@@ -16,10 +16,12 @@ def merge_dicts_by_key(*dicts: dict) -> dict:
                 merged_dict[key].extend(list(value))
             else:
                 merged_dict[key].append(value)
-    return dict(merged_dict)
+    return merged_dict
 
 
-def af_custom_callbacks(config: Config) -> tuple[dict[str, list[Optional[callable]]], dict[str, list[Optional[callable]]]]:
+def collect_af_custom_callbacks(
+    config: Config,
+) -> tuple[dict[str, list[Optional[callable]]], dict[str, list[Optional[callable]]]]:
     mcd_dag_callbacks, mcd_task_callbacks = prepare_mcd_callbacks(config)
     airflow_dag_callbacks, airflow_task_callbacks = prepare_custom_af_callbacks(config)
 
