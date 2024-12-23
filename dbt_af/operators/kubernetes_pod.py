3a63cf188c9e7346a6d5d30198b6d169386f3405
Nikita Yurasov <49479190+NikitaYurasov@users.noreply.github.com>
2024-12-23 10:57:35 +0100

    feat: add dbt executable path to config (#49)
    Features:
    - specify custom dbt executable path in the main config for all dbt
    tasks (https://github.com/Toloka/dbt-af/issues/46)
    
    Fixes:
    - make less strict dependencies constrains
    (https://github.com/Toloka/dbt-af/issues/45)

--- dbt_af/operators/kubernetes_pod.py
+++ dbt_af/operators/kubernetes_pod.py
@@ -7,7 +7,12 @@ from typing import TYPE_CHECKING
 
 import kubernetes.client.models as k8s
 from airflow.configuration import conf
-from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+
+# this is a workaround to be compatible with old versions of Airflow and cncf-kubernetes provider
+try:
+    from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+except (ModuleNotFoundError, ImportError):
+    from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
 
 from dbt_af.common.constants import AZ_MI_BINDING_LABEL_NAME
 from dbt_af.conf import Config
